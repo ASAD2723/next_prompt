6750ffe8a75a7d743e6ac3d9965d21219ee92551
ASAD2723 <khanasad323@gmail.com>
2023-06-17 05:44:34 +0530

    pause project

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,20 +8,20 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
   
-  const isUserLoggedIn = true
+  const { data: session} = useSession();
 
   const [ providers, setProviders ] = useState(null);
 
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   })
   
   return (
@@ -33,8 +33,10 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+
+
       <div className="sm:flex hidden">
-          {isUserLoggedIn ? (
+          {session?.user ? (
             <div className="flex gap-3 md:gap-5">
               <Link href="/create-prompt" className="black_btn">
                 Create Post
@@ -46,7 +48,7 @@ const Nav = () => {
 
               <Link href="/profile">
                 <Image
-                  src="/assets/images/logo.svg"
+                  src={session?.user.image}
                   width={37}
                   height={37}
                   className="rounded-full"
@@ -71,10 +73,10 @@ const Nav = () => {
       </div>
 
       <div className="sm:hidden flex relative"> 
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
